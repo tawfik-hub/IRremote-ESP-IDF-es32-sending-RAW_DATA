80c65b65ab01e9dfc34611202a84fc4fc97f35c4
Tawfik Aoun <tawfiknaoun@gmail.com>
2023-02-09 10:25:46 +0100

    esp idf v4 read ra_data

--- main/irSendAcCtrl.c
+++ main/irSendAcCtrl.c
@@ -1,3 +1,14 @@
+/* RMT receive example, displaying raw RMT data
+ * 
+ * This code was adapted from example: rmt_nec_tx_rx
+ * That software is distributed under Public Domain (or CC0 licensed, at your option.)
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+*/
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -7,176 +18,127 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "driver/rmt.h"
-#include "driver/rmt_tx.h"
-#include "driver/rmt_rx.h"
-#include "ir_nec_encoder.h"
-//I used a base project to write my code. I don't know if it's the RTM module best practices, but it worked for my AC
+#include "driver/periph_ctrl.h"
+#include "soc/rmt_reg.h"
 
-static const char* LOG_TAG = "TEST_LOG";
+// FreeRTOS function
+#define INCLUDE_vTaskDelay 1
 
-//To recording the ON and OFF values I used Arduino IDE with IRLibRecvPCI.h, using a Arduino UNO and I also tested it
-//with the ESP32 using IRrecv.h/ IRremoteESP8266.h
-//I didn't try to record the IR values using RMT, just because I already have the IR values.
+#define rrmt_item32_tIMEOUT_US  9500   /*!< RMT receiver timeout value */
 
-//Recorded command of each value for ON.
-uint16_t powerOn[]=
-{
-  8438,4026,466,1500,466,455,494,455,
-495,454,497,1501,467,455,494,454,
-495,456,494,485,466,455,494,486,
-466,454,521,459,466,482,468,1500,
-467,1500,467,455,495,454,496,454,
-494,1501,469,452,496,1500,467,457,
-493,1500,468,1499,468,455,494,454,
-519,1476,493,1000
+// LED on Huzzah32 board
+const int LED_BUILTIN = 13;
+
+// structure used to initialize RMT inputs
+// NOTE: tag is used on the monitor outputs to distinguish between channels
+struct rmt_rx_inits {
+	rmt_channel_t  channel;
+	gpio_num_t     gpio_num;
+	uint8_t        clk_div;
+	uint8_t        mem_block_num;
+	bool           config_filter_en;
+	uint8_t        config_filter_tics_thresh;
+	uint16_t       config_idle_threshold;
+	char           tag[10];
+}rx_inputs[2] = {
+	{ 0, 26, 80, 1, false, 0, 50000, "Ch0" },
+	{ 1, 5, 80, 1, false, 0, 50000, "Ch1" }
 };
-/*uint16_t powerOn[] = 
-{
-3658, 1554, 442, 1350,422, 1350,422, 638, 
-  406, 618, 422, 642, 434, 1294,478, 590, 
-  454, 606, 438, 1334,434, 1298,474, 586, 
-  422, 1330,478, 566, 474, 586, 458, 1294, 
-  474, 1298,474, 586, 458, 1314,454, 1318, 
-  454, 586, 458, 586, 454, 1294,478, 606, 
-  434, 586, 458, 1294,478, 586, 454, 586, 
-  458, 582, 458, 582, 458, 586, 458, 582, 
-  458, 562, 482, 586, 454, 566, 478, 586, 
-  454, 586, 458, 586, 454, 562, 482, 586, 
-  454, 562, 482, 586, 458, 582, 458, 1314, 
-  458, 586, 454, 562, 482, 1314,458, 562, 
-  478, 582, 458, 1314,458, 1314,458, 586, 
-  454, 586, 458, 586, 458, 562, 478, 562, 
-  478, 586, 458, 1282,490, 562, 482, 582, 
-  458, 1334,438, 586, 454, 562, 482, 562, 
-  478, 566, 474, 562, 482, 1334,434, 566, 
-  478, 1314,458, 562, 478, 582, 458, 566, 
-  478, 582, 458, 606, 438, 586, 454, 586, 
-  458, 582, 458, 558, 482, 562, 482, 582, 
-  458, 566, 478, 582, 458, 586, 458, 602, 
-  438, 582, 458, 586, 454, 566, 478, 558, 
-  482, 562, 482, 1290,478, 586, 458, 562, 
-  478, 562, 482, 582, 458, 602, 438, 582, 
-  458, 566, 478, 602, 438, 562, 478, 1314, 
-  458, 562, 482, 562, 478, 562, 478, 562, 
-  478, 566, 478, 558, 482, 562, 482, 1310, 
-  458, 566, 478, 1294,474, 558, 486, 1294, 
-  474, 586, 458, 1000
-  };*/
-  
-//Recorded command of each value for OFF.
-uint16_t poweroff[] =  
-{
-3694, 1478, 518, 1314,458, 1294,478, 582, 
-  458, 586, 458, 586, 458, 1314,458, 582, 
-  458, 586, 458, 1310,458, 1294,478, 586, 
-  458, 1314,458, 586, 454, 606, 438, 1314, 
-  458, 1290,482, 582, 458, 1314,458, 1294, 
-  478, 562, 478, 586, 458, 1290,482, 586, 
-  454, 586, 458, 1294,478, 582, 458, 570, 
-  474, 566, 478, 582, 458, 582, 458, 586, 
-  458, 558, 482, 562, 478, 590, 454, 566, 
-  478, 602, 438, 602, 442, 562, 478, 582, 
-  458, 586, 458, 582, 458, 562, 482, 582, 
-  458, 562, 482, 582, 458, 1314,458, 602, 
-  438, 602, 438, 1334,438, 1294,478, 606, 
-  434, 586, 458, 562, 478, 586, 458, 562, 
-  478, 586, 458, 1310,462, 582, 458, 562, 
-  478, 1290,482, 582, 458, 590, 454, 558, 
-  482, 586, 458, 566, 474, 1318,454, 562, 
-  478, 1290,482, 586, 458, 562, 478, 582, 
-  462, 582, 458, 558, 482, 582, 458, 566, 
-  478, 586, 454, 562, 482, 562, 478, 586, 
-  458, 586, 458, 578, 462, 562, 478, 582, 
-  462, 558, 482, 582, 458, 562, 482, 558, 
-  482, 582, 458, 1314,458, 582, 458, 586, 
-  458, 558, 482, 586, 458, 562, 478, 582, 
-  458, 582, 458, 558, 486, 582, 458, 1314, 
-  458, 582, 462, 582, 458, 582, 458, 586, 
-  458, 582, 458, 566, 474, 562, 482, 582, 
-  458, 562, 482, 1310,458, 562, 482, 1286, 
-  482, 586, 458, 1000
-  };
-  
-#define DataLength 228    						//Length of powerOn (or powerOff, It has the same size)
 
-const rmt_channel_t RMT_CHANNEL = RMT_CHANNEL_0;
-const gpio_num_t IR_PIN = GPIO_NUM_27;			//Using GPIO 5 of ESP32
-const int  RMT_TX_CARRIER_EN = 1;   			//Enable carrier for IR transmitter test with IR led
-
-//The setup I left "default" of the code I used as a base, just changing "rmtConfig.tx_config.carrier_freq_hz = 36000;" which was 
-//38000 but I don't know if is necessary to change it
-void setup_rmt_config() 
-{
-	//put your setup code here, to run once:
-	rmt_config_t rmtConfig;
-
-	rmtConfig.rmt_mode = RMT_MODE_TX;  								//transmit mode
-	rmtConfig.channel = RMT_CHANNEL;  								//channel to use 0 - 7
-	rmtConfig.clk_div = 80;  										//clock divider 1 - 255. source clock is 80MHz -> 80MHz/80 = 1MHz -> 1 tick = 1 us
-	rmtConfig.gpio_num = IR_PIN; 									//pin to use
-	rmtConfig.mem_block_num = 1; 									//memory block size
-
-	rmtConfig.tx_config.loop_en = 0; 								//no loop
-	rmtConfig.tx_config.carrier_freq_hz = 36000;  					//36kHz carrier frequency
-	rmtConfig.tx_config.carrier_duty_percent = 33; 					//duty
-	rmtConfig.tx_config.carrier_level =  RMT_CARRIER_LEVEL_HIGH; 	//carrier level
-	rmtConfig.tx_config.carrier_en =RMT_TX_CARRIER_EN ;  			//carrier enable
-	rmtConfig.tx_config.idle_level =  RMT_IDLE_LEVEL_LOW ; 			//signal level at idle
-	rmtConfig.tx_config.idle_output_en = true;  					//output if idle
-
-	rmt_config(&rmtConfig);
-	rmt_driver_install(rmtConfig.channel, 0, 0);
+// initialize RMT receive channels
+void rx_channels_init() {
+    rmt_config_t rmt_rx;
+    size_t i;
+    size_t len = sizeof(rx_inputs) / sizeof( rx_inputs[0] );
+    
+    for ( i=0; i<len; i++ ) {
+		rmt_rx.channel       = rx_inputs[i].channel;
+		rmt_rx.gpio_num      = rx_inputs[i].gpio_num;
+		rmt_rx.clk_div       = rx_inputs[i].clk_div;
+		rmt_rx.mem_block_num = rx_inputs[i].mem_block_num;
+		rmt_rx.rmt_mode      = RMT_MODE_RX;
+		
+		rmt_rx.rx_config.filter_en           = rx_inputs[i].config_filter_en;
+		rmt_rx.rx_config.filter_ticks_thresh = rx_inputs[i].config_filter_tics_thresh;
+		rmt_rx.rx_config.idle_threshold      = rx_inputs[i].config_idle_threshold;
+		
+		rmt_config(&rmt_rx);
+		rmt_driver_install(rx_inputs[i].channel, 1000, 0);
+	}
 }
 
-static void rmtAcCtrlTask()
-{
-    vTaskDelay(10);
-    setup_rmt_config();
-    esp_log_level_set(LOG_TAG, ESP_LOG_INFO);
+// initialize visible LED on ESP32 board
+/*static void visible_led_init() {
+    gpio_pad_select_gpio(LED_BUILTIN);
+	gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+}*/
 
-	const int sendDataLength = DataLength/2;        //sendDataLength is the range of the array powerOn and powerOff It is divided by two
-													//because each index of object sendDataOn holds High and Low values and the powerOn/powerOff
-													//array uses an index for High and other for Low
-													
-	rmt_item32_t sendDataOn[sendDataLength]; 		//Data to send the Ac turn On 
+/* Converts the RMT level, duration into a positive or negative integer
+ * Compatible with the ESP32-RMT-server application
+ * Note: most IR receivers have active-low outputs, where the
+ *   ESP32-RMT-server application has active-high oututs
+ * This function inverts the RMT receive level so the text output is
+ *   compatible with ESP32-RMT-server application
+ */
+int dur( uint32_t level, uint32_t duration ) {
+	if ( level == 0 ) { return duration; }
+	else { return 1.0 * duration; }
+}
 
-	for(int i = 0, t = 0; i < (sendDataLength*2)-1; i += 2, t++)
-	{
-								//Odd bit High
-		sendDataOn[t].duration0 = powerOn[i];		//The patern is odd bits to High on IR LED and even bits to Low  
-		sendDataOn[t].level0 = 1;					//looking at the powerOn and Off array index. So this is mapped on 
-								//Even bit Low		//sendDataOn for every index
-		sendDataOn[t].duration1 = powerOn[i+1];
-		sendDataOn[t].level1 = 0;
-	}
+// RMT receiver task
+static void rmt_example_nec_rx_task() {
+	size_t num_channels = sizeof(rx_inputs) / sizeof( rx_inputs[0] );
+	size_t c, i;
+    size_t rx_size = 0;
+    rmt_item32_t* items = NULL;
 	
-	rmt_item32_t sendDataOff[sendDataLength]; 		//Data to send the Ac turn Off 
-
-	for(int i = 0, t = 0; i < (sendDataLength*2)-1; i += 2, t++)
-	{
-								//Odd bit High
-		sendDataOff[t].duration0 = poweroff[i];
-		sendDataOff[t].level0 = 1;
-								//Even bit Low
-		sendDataOff[t].duration1 = poweroff[i+1];
-		sendDataOff[t].level1 = 0;
+    // define ringbuffer handle
+    RingbufHandle_t rb;
+	
+    // start receiving IR data
+    for ( c=0; c<num_channels; c++ ) {
+		rmt_rx_start(rx_inputs[c].channel, 1);
 	}
-
-    while(1) 
-	{
-		ESP_LOGI(LOG_TAG, "SEND RMT DATA");
-		
-		//Just to see it working I set this task to every 5 seconds send the AC turn On and Off
-		rmt_write_items(RMT_CHANNEL, sendDataOn, sendDataLength, false);
-		vTaskDelay(10000 / portTICK_PERIOD_MS); //5 sec delay
-		
-		/*rmt_write_items(RMT_CHANNEL, sendDataOff, sendDataLength, false);
-		vTaskDelay(5000 / portTICK_PERIOD_MS); //5 sec delay*/
-    }
-    vTaskDelete(NULL);
+    
+    // loop forever
+    while (1) {
+		// check each receive channel
+		for ( c=0; c<num_channels; c++ ) {
+			// get the ring buffer handle
+			rmt_get_ringbuf_handle(rx_inputs[c].channel, &rb);
+			
+			// get items, if there are any
+			items = (rmt_item32_t*) xRingbufferReceive(rb, &rx_size, 10);
+			if(items) {
+				// turn on visible led
+				gpio_set_level(LED_BUILTIN, 1);
+				
+				// print the RMT received durations to the monitor
+				printf( "  %s received %i items\n", rx_inputs[c].tag, rx_size/4 );
+				for ( i=0; i<rx_size/4; i++ ) {
+					if ( i>0 ) { printf(","); }
+					printf( "%i", dur( items[i].level0, items[i].duration0 ) );
+					printf(",%i", dur( items[i].level1, items[i].duration1 ) );
+				}
+				printf("\n");
+				
+				// turn off visible led
+				gpio_set_level(LED_BUILTIN, 0);
+				
+				// free up data space
+				vRingbufferReturnItem(rb, (void*) items);
+			}
+		}
+		// delay 100 milliseconds. No need to overheat the processor
+		vTaskDelay( 100 / portTICK_PERIOD_MS );
+	}
 }
 
-void app_main()
-{
-	xTaskCreate(rmtAcCtrlTask, "rmtAcCtrlTask", 10000, NULL, 10, NULL);
+void app_main() {
+	// initialize hardware
+    rx_channels_init();
+    //visible_led_init();
+    
+    // start receive processing task
+    xTaskCreate(rmt_example_nec_rx_task, "rmt_nec_rx_task", 2048, NULL, 10, NULL);
 }
